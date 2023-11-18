8253dfad8ca0b02364fcd4cff0ce437eab05aa92
Stratos Kavalikas <kavalikello@gmail.com>
2023-11-18 13:18:19 +0200

    Initialisation of project

---
+++ OnlineShop/OnlineShop/OnlineShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnlineShopApp.swift
+//  OnlineShop
+//
+//  Created by Stratos Lik on 18/11/23.
+//
+
+import SwiftUI
+
+@main
+struct OnlineShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
